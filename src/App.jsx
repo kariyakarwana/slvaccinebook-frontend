c494e286a85842f10ab19225c27957c44e384535
kariyakarwana <kariyakarawanaz@gmail.com>
2025-09-06 14:10:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline text-blue-500">
+      Hello world!
+    </h1>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
